af81127b0ca9ef45e8c9393431378e7709d55d7d
russell-cook <russ.cook@gmail.com>
2013-10-04 17:09:05 -0700

    Initialize Repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ppbb::Application.config.secret_key_base = '51ebc0364473ab3701b2634476673ba940495006a18197106eef3d69046aea0041e7e43225a9034d9d07c39dc9b7bb3044c299c38ddc62d9d13c718a5675965c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ppbb::Application.config.session_store :cookie_store, key: '_ppbb_session'
